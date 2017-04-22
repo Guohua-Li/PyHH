7aa553af005e401554d726accf7674f0e2d8ec93
Guohua-Li <ligh07@126.com>
2017-04-21 20:38:58 -0400

    Add files via upload

--- tutorial-2.py
+++ tutorial-2.py
@@ -1,15 +1,11 @@
 from pyhh import *
 
-soma = Compartment(50,length=None)
-dend = Compartment(1.5,length=100)
-
-soma.add_channels(NaC, KDR, gL)
-dend.add_channels(NaC, KDR, gL)
-
-soma.connect(dend) # or dend.attached_to(soma)
+soma = Compartment(50, None, [NaC, KDR, gL])
+dend = Compartment(1.5, 100, [NaC, KDR, gL])
+dend.attached_to(soma)
 
 soma.add_iclamper()
-soma.iClamper.Waveform = Rect(delay=2, width=0.5, amplitude=1.45)
+soma.iClamper.Waveform = Alpha(delay=2, tau=0.5, amplitude=0.9)
 
 xp = Experiment([soma,dend])
 xp.run(10, 0.002)
