bf184c7b4a8ac359fe30a61e3a85c022ff55be70
Guohua-Li <ligh07@126.com>
2017-04-21 20:40:17 -0400

    Add files via upload

--- tutorial-3.py
+++ tutorial-3.py
@@ -1,17 +1,13 @@
 from pyhh import *
 
-
 N = 50   # number of compartment to be created
 D = 1.5  # diameter
 L = 50   # length
 
-P = [ Compartment(D, L) for i in range(N) ]
+P = [ Compartment(D, L, [NaC, KDR, gL]) for i in range(N) ]
 
 for i in range(N-1):
   P[i].connect(P[i+1])
-
-for cpm in P:
-  cpm.add_channels(NaC, KDR, gL)
 
 P[0].add_iclamper()
 P[0].iClamper.Waveform = Rect(delay=1.5, width=1., amplitude=1.65)
@@ -19,14 +15,4 @@ P[0].iClamper.Waveform = Rect(delay=1.5, width=1., amplitude=1.65)
 xp = Experiment(P)
 xp.run(30, 0.002)
 xp.plot()
-
-"""
-import matplotlib.pyplot as plt
-fig, ax1 = plt.subplots()
-for cpm in P:
-  ax1.plot(xp.T, cpm.Vm, linewidth=2.0)
-
-ax2 = ax1.twinx()
-ax2.plot(xp.T, P[0].iClamper.Command, 'r-')
-"""
 
