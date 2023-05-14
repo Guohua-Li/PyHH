4da0ca1a4a39344b120efc9e3e2da597da64a1d3
Guohua-Li <licharles35@gmail.com>
2023-05-14 11:28:15 -0400

    Update tutorial-1.py

--- tutorial-1.py
+++ tutorial-1.py
@@ -1,9 +1,15 @@
-from pyhh import *
-cpm = Compartment(diameter=1.5, length=100)
-cpm.add_channels(NaC, KDR, gL)
-cpm.add_iclamper()
-cpm.iClamper.Waveform = Rect(delay=1, width=1, amplitude=0.55)
-xp = Experiment(cpm)
-xp.run(t=10, dt=0.005)
-cpm.plot()
+from compartment import Compartment
+from channels import NaC, KDR, gL
+from clampers import Rect
+from experiment import Experiment
 
+
+#cell = Compartment(diameter=1.5, length=100)
+cell = Compartment(diameter=1.5, length=None)
+cell.add_channels(NaC, KDR, gL)
+cell.add_iclamper()
+cell.iClamper.Waveform = Rect(delay=1, width=1, amplitude=0.55)
+
+xp = Experiment(cell)
+xp.run(t = 10) # , dt=0.005
+cell.plot()
