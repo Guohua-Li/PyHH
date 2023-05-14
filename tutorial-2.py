e30bca8bedac576eabbd103883411254e832e3a6
Guohua-Li <licharles35@gmail.com>
2023-05-14 11:31:23 -0400

    Update tutorial-2.py

--- tutorial-2.py
+++ tutorial-2.py
@@ -1,11 +1,15 @@
-from pyhh import *
+from compartment import Compartment
+from channels import NaC, KDR, gL
+from clampers import Alpha
+from experiment import Experiment
+
 
 soma = Compartment(50, None, [NaC, KDR, gL])
 dend = Compartment(1.5, 100, [NaC, KDR, gL])
 dend.attached_to(soma)
 
-soma.add_iclamper()
-soma.iClamper.Waveform = Alpha(delay=2, tau=0.5, amplitude=0.9)
+clamper = soma.add_iclamper()
+clamper.Waveform = Alpha(delay=2, tau=0.5, amplitude=0.9)
 
 xp = Experiment([soma,dend])
 xp.run(10, 0.002)
@@ -17,22 +21,3 @@ xp.plot()
 xp.Clock = 0
 xp.run(10, 0.005)
 xp.plot()
-
-"""
-# make your own plot:
-import pylab as plt
-plt.figure()
-plt.subplot(2,1,1)
-plt.plot(xp.T, soma.Vm)
-plt.plot(xp.T, dend.Vm, linewidth=2.0)
-plt.ylim([-80,30])
-plt.ylabel('V (mV)')
-
-plt.subplot(2,1,2)
-plt.plot(xp.T, soma.iClamper.Command, linewidth=2.0)
-plt.ylim([-1,2])
-plt.xlabel('time (ms)')
-plt.ylabel('current (pA/um2)')
-plt.show()
-"""
-
