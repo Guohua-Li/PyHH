21cf3f15462d79a0bef7a419dccd8ccd5eae5316
Guohua-Li <licharles35@gmail.com>
2023-05-21 22:00:29 -0400

    Update lgic.py

--- lgic.py
+++ lgic.py
@@ -8,123 +8,119 @@ class Ligand:
     self.Conc = concentration
     self.Clamper = None
 
-class TransRate:
-  def __init__(self, func, category='V'):
-    self.f = func
+
+class Transition:
+  def __init__(self, func, category = 'V'):
     self.Rate = 0
+    self.f = func
     self.Compt = None
     self.Category = category
+    self.Tag = "Transition"
 
   def update(self):
     V = self.Compt.Vi
     self.Rate = self.f(V)
+
 
 class LGIC:
   """
   LGICs based on Markov transition schemes.
   """
   is_LGIC = 1
-  def __init__(self, transit, binding, gMax, ER):
+  def __init__(self, transit, binding):
     self.transit = transit # keep it for copying the channel
     self.binding = binding # keep it for copying the channel
-    self.gMax = gMax
-    self.ER   = ER
     self.Tag = 'LGIC'
     self.ptr = None
 
     self.States = list(transit.keys()) # 2.x and 3.y are different
     self.States.sort()
     self.nStates = len(self.States)
-    self.do_parsing(transit, binding)
-    self._get_rate_matrix()
-    self.N = len(self.RM)
-    self.Prob = [0]*len(self.RM) # initialize the state probability vector
+    self.Transit = self.do_parsing(transit, binding)  # states in number, basically a sparse matrix in dic form
+
+    self.OpenStateNumbers = [ k for k in range(self.nStates) if self.States[k][0] == 'O' ]
+    self.LigandGatedDict = self.get_bind_paths(binding)
+    """
+    example of self.LigandGatedDict
+    {<lgic.Ligand object at 0x7f156a967ee0>: [(0, 1), (1, 2)]}
+    where 0->1, 1->2 are the paths for ligand binding
+    """
+
+    self.Prob = [0] * self.nStates # initialize the state probability vector
     self.Prob[0] = 1.0
+
     self.Ligands = list(binding.keys())
     if len(self.Ligands) == 1: self.Ligand = self.Ligands[0]
 
-  def _set_gMax(self, val):
-    self.gMax = val
+  def get_bind_paths(self, binding):
+    StateMapIndex = {key: i for i, key in enumerate(self.States)}
+    # dict(map(reversed, enumerate(self.States)))
 
-  def _set_ER(self, val):
-    self.gMax = val
-
-  def do_parsing(self, transit, binding):
-    StateIndexDict = {}
-    for k in range(self.nStates):
-      s = self.States[k]
-      StateIndexDict[s] = k
-
-    self.Transit = {}
-    for i in range(self.nStates):
-      si = self.States[i]
-      for j in range(self.nStates):
-        sj = self.States[j]
-        if sj in transit[si].keys():
-          if i in self.Transit:
-            self.Transit[i].update({ j:transit[si][sj] })
-          else:
-            self.Transit[i] = { j:transit[si][sj] }
-        else:
-          if i in self.Transit:
-            self.Transit[i].update({ j:0 })
-          else:
-            self.Transit[i] = { j:0 }
-
-    self.GateDict = {}
+    m_dict = {}
     for lg in binding.keys():
       paths = []
-      scheme = binding[lg]
-      for a in scheme.keys():
-        b = scheme[a]
-        i = StateIndexDict[a]
-        j = StateIndexDict[b]
+      scheme = binding[lg] # scheme is a dict
+      for start in scheme.keys():
+        end = scheme[start]
+        i = StateMapIndex[start]
+        j = StateMapIndex[end]
         paths.append((i,j))
-      self.GateDict[lg] = paths
+      m_dict[lg] = paths
+    return m_dict
 
-    self.OpenIndex = []
-    for k in range(self.nStates):
-      if self.States[k][0] == 'O': self.OpenIndex.append(k)
-
-  def _get_rate_matrix(self):
-    N = self.nStates
-    states = range(N)
-    self.RM = [[1]*N for i in states] # like ones((N, N), float)
-    for i in states:
-      for j in states:
-        if isinstance(self.Transit[i][j], TransRate):
-          self.RM[i][j] = self.Transit[i][j].Rate
+  def do_parsing(self, transit, binding):
+    trans_dict = {} # states in number, basically a sparse matrix in dic form
+    state_numbers = range(self.nStates)
+    for i in state_numbers:
+      state_i = self.States[i]
+      for j in state_numbers:
+        state_j = self.States[j]
+        if state_j in transit[state_i].keys():
+          if i in trans_dict:
+            trans_dict[i].update({ j:transit[state_i][state_j] })
+          else:
+            trans_dict[i] = { j:transit[state_i][state_j] }
         else:
-          self.RM[i][j] = self.Transit[i][j]
+          if i in trans_dict:
+            trans_dict[i].update({ j: 0 })
+          else:
+            trans_dict[i] = { j: 0 }
+    return trans_dict
 
-  def _ProbOpen(self):
-    gate = 0.0
-    for k in self.OpenIndex: gate += self.Prob[k]
-    return gate
+  def _update_gate(self, V, dt): # used by xp, this is the solution of the ...
+    state_numbers = range(self.nStates)
+    rate_mat = [[0]*self.nStates for i in state_numbers] # like zeros/ones((N, N), float), N rows, N cols
+    for i in state_numbers:
+      for j in state_numbers:
+        if isinstance(self.Transit[i][j], Transition): # NMDAR will use this
+          rate_mat[i][j] = self.Transit[i][j].Rate # At the start, Rate = 0
+        else:
+          rate_mat[i][j] = self.Transit[i][j]
 
-  def _update_gate(self, V, h):
-    self._get_rate_matrix()
-    N = self.nStates
-    states = range(N)
-    for lg, paths in self.GateDict.items():
+
+    for lg, paths in self.LigandGatedDict.items():
       for s, t in paths:
-        self.RM[s][t] = self.RM[s][t] * lg.Conc
-    A = [sum(i) for i in self.RM]           # sum of each row
-    m = [ [j*self.Prob[i] for j in self.RM[i]] for i in states ] # self.RM * self.XC
+        rate_mat[s][t] = rate_mat[s][t] * lg.Conc
+
+    A = [sum(i) for i in rate_mat]           # sum of each row
+    m = [ [j*self.Prob[i] for j in rate_mat[i]] for i in state_numbers ] # self.RM * self.XC
     B = list(map(sum,zip(*m))) # sum of each vol, faster
-    for k in states:
+    for k in state_numbers:
       if A[k] == 0:
-        self.Prob[k] = self.Prob[k] + h * B[k]
+        self.Prob[k] = self.Prob[k] + dt * B[k]
       else:
         C = B[k]/A[k]
-        self.Prob[k] = (self.Prob[k] - C) * exp(-A[k]*h) + C
+        self.Prob[k] = (self.Prob[k] - C) * exp(-A[k]*dt) + C
     S = sum(self.Prob)
     if S != 0:
       self.Prob = [x/S for x in self.Prob]
+
+  def _ProbOpen(self): # only used by compartment
+    gate = 0.0
+    for k in self.OpenStateNumbers: gate += self.Prob[k]
+    return gate
 
   def show(self):
     print ("State Probabilities:")
     for k in range(self.nStates):
       print ("%5.4f"%(self.Prob[k]))
-
-
