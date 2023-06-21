83a4885fd741b914f18dd612cde66d25ffd2c15b
Guohua-Li <licharles35@gmail.com>
2023-06-21 10:24:25 -0400

    Update compartment.py

--- compartment.py
+++ compartment.py
@@ -1,6 +1,10 @@
-import copy
+# modified on May 19, 2023
 
-from lgic import LGIC, Transition
+import copy
+import array
+import math
+
+from lgic import LGIC, VariableRate
 from clampers import VClamper, IClamper
 from math import exp
 import matplotlib.pyplot as plt
@@ -8,8 +12,9 @@ import matplotlib.pyplot as plt
 COUNT = 0
 
 def increment():
-    global COUNT
-    COUNT = COUNT+1
+  global COUNT
+  COUNT += 1
+
 
 def isLGIC(x):
   return hasattr(x,'is_LGIC')
@@ -19,57 +24,50 @@ class Compartment:
   Compartment on which neurons are built.
   """
   is_Compartment = 1
-  def __init__(self, diameter = 20, length = 50, channels=None, parent=None):
+  def __init__(self, diameter = 20, length = None, channels=None, parent=None):
     self.V0    = -61.237
     self.Vi    = self.V0 # Vi: intracellular potential of the compartment, just a single value
     self.Vm    = None # an array.array to store membrane, modified in the Experiment class
     self.Vdot  = 0.0
-    self.Cm    = 0.01 # Specific membrance capacitance:      pF / um2
+    self.Cm    = 0.01 # Specific membrance capacitance: pF/um2
     self.Gx    = 700.0
-    # cytoplasm conductivity: the reciprocal of electrical resistivity
-    # or speciﬁc electrical resistance for the cytoplasm modelled as a resistor
-    # The new value is taken from 
-    # Wang, K., Zhao, Y., Chen, D. et al. Specific membrane capacitance, cytoplasm conductivity and
-    # instantaneous Young’s modulus of single tumour cells. Sci Data 4, 170015
-    # (2017). https://doi.org/10.1038/sdata.2017.15
-    #self.Gx    = 958.0 # average of values from 5 types of cell, the unit is ns/um
+
     self.Length = length
     self.Diameter = diameter
     self.iClamper = None
     self.vClamper = None
     self.cClamper = None
-    self.iMonitor = None
+    self.iRecorder = None
 
     self.Parent = None
     if parent != None:
       self.attached_to(parent)
 
     self.Children = []
-    self.RateList = []
+    self.VoltGatedRates = [] #RateList
     self.ID = COUNT
     increment()
 
-    # newly define the make the program clear
     # J_axial is not used in the current class
     self.J_axial = 0.0 # axial current density, direction is from the child to parent, see Experiment class
 
     # J_total is used in the current class for updating Vi
     self.J_total = 0.0 # all current injected into the segment from neighboring compartments and IClamper
 
-    self.Density = {} # testing
-    self.Reversal = {} #testing
+    self.Density = {}
+    self.Reversal = {}
 
     self.channel_list = []
     if channels != None:
       for ch_obj in channels.keys():
-        copied = self._add_channel_copy(ch_obj) # copy and add
+        copied = self._add_channel_copy(ch_obj) # copy and add to channel_list
         self.Density[copied]  = channels[ch_obj][0]
         self.Reversal[copied] = channels[ch_obj][1]
 
   def add_channels(self, channels): # testing, seem ok
     L = []
     for ch_obj in channels.keys():
-      copied = self._add_channel_copy(ch_obj) # copy and add
+      copied = self._add_channel_copy(ch_obj) # copy and add to channel_list
       self.Density[copied]  = channels[ch_obj][0]
       self.Reversal[copied] = channels[ch_obj][1]
       L.append(copied)
@@ -77,23 +75,28 @@ class Compartment:
     if len(L)==1: return L[0]
     return tuple(L)
 
+  def get_channel(self, tag):
+    for chnn in self.channel_list:
+      if chnn.Tag == tag:
+        return chnn
+
   def _add_channel_copy(self, Channel):
     if isLGIC(Channel):
       Channel.transit = copy.deepcopy(Channel.transit)
       Channel.binding = copy.deepcopy(Channel.binding)
       ch_obj = LGIC(Channel.transit, Channel.binding)
       """for i in ch_obj.Transit:
-        if isinstance(i, Transition):
+        if isinstance(i, VariableRate):
           i.Ptr = self
-          self.RateList.append(i)"""
-
+          self.VoltGatedRates.append(i) # RateList
+          """
       state_numbers = range(ch_obj.nStates)
       for state_i in state_numbers:
         for state_j in state_numbers:
-          x = ch_obj.Transit[state_i][state_j] # x is a number or a Transition object
-          if isinstance(x, Transition):
+          x = ch_obj.Transit[state_i][state_j] # x is a number or a VariableRate object
+          if isinstance(x, VariableRate):
             x.Compt = self
-            self.RateList.append(x) # only the voltageGate
+            self.VoltGatedRates.append(x) # RateList
     else: # non-lgic
       ch_obj = copy.deepcopy(Channel)
 
@@ -177,44 +180,43 @@ class Compartment:
     ch.ptr = self
     return ch
 
-  def get_Jion(self, V):
-    # this is required by voltage clamp,
-    # not current clamp
+  def get_Jion(self, V, step):
+    # this is required only by voltage clamp
     Jion = 0.0
     for ch_obj in self.channel_list:
-      g = self.Density[ch_obj] * ch_obj._ProbOpen() # ch_obj.gMax
+      p = ch_obj._ProbOpen(step)
+      g = self.Density[ch_obj] * p # ch_obj.gMax
       Jion += g * (V - self.Reversal[ch_obj]) # ch_obj.ER
     return Jion
 
-  def add_iclamper(self):
+  def add_iclamper(self, waveform=None, delay=None, amplitude=None, width=None, tau=None, rlist=None, interval=None, number=None): #
     clamper = IClamper()
     self.iClamper = clamper
+    if waveform != None:
+      clamper.set_waveform(waveform, delay, amplitude, width, tau, rlist, interval, number)
     return clamper
 
-  def add_vclamper(self, baseline):
-    clamper = VClamper(baseline)
+  def add_vclamper(self, baseline, waveform=None, delay=None, amplitude=None, width=None, tau=None, rlist=[]):
+    clamper = VClamper(self, baseline)
+    if waveform != None: clamper.set_waveform(waveform, delay, amplitude, width, tau, rlist=[])
     self.vClamper = clamper
     return clamper
 
-  def add_imonitor(self):
-    monitor = IMonitor()
-    self.iMonitor = monitor
-    return monitor
-
   def _calc_surface(self):
-    if self.Length == None:
-      self.Surface = 3.1415926 * (self.Diameter**2) # sphere
-      return
-
-    self.Surface = 3.1415926 * self.Diameter * self.Length
-    S = 3.1415926 * self.Diameter * self.Diameter / 4.0 # this is the circle area
-    if self.Parent:
-      if self.Children == []:
-        self.Surface = self.Surface + S # terminal segment?
+    if self.Length == None: # sphere
+      self.Surface = 3.1415926 * (self.Diameter**2) # sphere area
+      for child in self.Children:
+        self.Surface -= 3.1415926 * child.Diameter * child.Diameter / 4.0
     else:
-      self.Surface = self.Surface + S # one end is closed
-      if self.Children == []:
-        self.Surface = self.Surface + S # the other end is closed
+      self.Surface = 3.1415926 * self.Diameter * self.Length # curved surface area
+      base_area = 3.1415926 * self.Diameter**2 / 4.0
+      if self.Parent:
+        if self.Children == []:
+          self.Surface += base_area # This is a terminal segment.
+      else:
+        self.Surface += base_area # one end is closed
+        if self.Children == []:
+          self.Surface += base_area # the other end is closed too
 
   def _calc_gx(self):
     if self.Length:
@@ -236,17 +238,25 @@ class Compartment:
     parent_cmpt.Children.append(self)
     self.Parent = parent_cmpt
 
-  def _update_Vm(self, dt):
+  def _update_Vm(self, dt, step):
     if self.channel_list == []: # passive circuit
       self.Vi = self.Vi + dt * self.J_total / self.Cm # Specific membrance capacitance: pF/um2
       return
 
     sigma_g  = sigma_gE = 0.0
     for ch_obj in self.channel_list:
-      g = self.Density[ch_obj] * ch_obj._ProbOpen() # g: unit area conductance, ch_obj.gMax
+      p = ch_obj._ProbOpen(step)
+      g = self.Density[ch_obj] * p # g: unit area conductance, ch_obj.gMax
       sigma_g += g
       sigma_gE += g * self.Reversal[ch_obj] #ch_obj.ER
-
+      """
+      if ch_obj.record_mode=="none": continue
+      if   ch_obj.record_mode == "g":
+        ch_obj.trace[step] = g
+        ptint("g=",g)
+      elif ch_obj.record_mode == "p":
+        ch_obj.trace[step] = p
+      """
     V0 = self.Vi
     """if sigma_g == 0:
       self.Vi = V0 + dt * (self.J_total+sigma_gE) / self.Cm
@@ -271,28 +281,98 @@ class Compartment:
   def show(self):
     print ('Vm =%f' % (self.Vi))
 
-  def plot_Vm(self, vlimit=None):
-    M, m = max(self.Vm), min(self.Vm)
-    if vlimit == None and M > m:
-      R = (M - m)/10
-      vlimit = [m-R,M+R]
+  def _plot_Vm(self, xlim, ylim, show_density=True):
+    N = len(self.Vm)
+    if N == 0: return
 
     fig = plt.figure()
-    if self.iClamper: # we need to show the command
-      ax1 = plt.subplot2grid((4,1), (0,0), rowspan=3)
-      ax2 = plt.subplot2grid((4,1), (3,0))
-      ax1.plot(self.T, self.Vm, linewidth=1.0)
-      ax2.plot(self.T, self.iClamper.Command, linewidth=1.0)
-      if vlimit != None:
-        ax1.set_ylim(vlimit)
-      ax2.set_xlabel('time (ms)')
-      ax2.set_ylim([0,1.1*self.iClamper.Waveform.Amplitude])
-    else:
-      N = len(self.Vm)
-      if N == 0: return
-      plt.plot(self.T, self.Vm, linewidth=2.0)
-      plt.xlabel('time (ms)')
-      plt.ylabel('mV')
-      if vlimit != None:
-        plt.ylim(vlimit)
+    """plt.plot(self.T, self.Vm, linewidth=1.0)
+    plt.xlabel('ms')
+    plt.ylabel('mV')
+    plt.xlim(xlim)
+    plt.ylim(ylim)"""
+
+    ax = fig.add_subplot(111)
+    ax.tick_params(axis='y', labelcolor='tab:blue')
+    ax.plot(self.T, self.Vm, linewidth=1.0)
+    ax.set_xlabel('ms')
+    ax.set_ylabel('mV', color='tab:blue')
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.tick_params(axis='y', colors='tab:blue')
+    ax.spines['left'].set_color('tab:blue')
+
+    #ax.spines['bottom'].set_color('red')
+    #ax.spines['top'].set_color('red')
+    #ax.xaxis.label.set_color('red')
+    #ax.tick_params(axis='x', colors='red')
+
     plt.show()
+
+  def _plot_Vm_clamper(self, xlim, ylim, show_density=True):
+    fig = plt.figure()
+    if show_density:
+      i_cmd = self.iClamper.Command
+    else:
+      N = len(self.iClamper.Command)
+      i_cmd = array.array('f', [0]) * N
+      for i in range(N):
+        i_cmd[i] = self.iClamper.Command[i] * self.Surface/1000.
+
+    ax1 = plt.subplot2grid((4,1), (0,0), rowspan=3)
+    ax1.plot(self.T, self.Vm, linewidth=1.0)
+    ax1.set_ylim(ylim)
+    ax1.set_xlim(xlim)
+    ax1.set_ylabel('mV')
+    ax1.yaxis.label.set_color('tab:blue')
+    #ax1.tick_params(axis='y', labelcolor='tab:blue')
+    ax1.tick_params(axis='y', colors='tab:blue')
+    ax1.spines['left'].set_color('tab:blue')
+    ax1.spines['right'].set_color('tab:gray')
+    ax1.spines['top'].set_color('tab:gray')
+    ax1.spines['bottom'].set_color('tab:gray')
+
+    ax2 = plt.subplot2grid((4,1), (3,0))
+    ax2.plot(self.T, i_cmd, linewidth=1.0)
+    ax2.set_xlim(xlim)
+    ax2.set_xlabel('ms')
+    ax2.yaxis.label.set_color('tab:blue')
+    #ax2.tick_params(axis='y', labelcolor='tab:blue')
+    ax2.tick_params(axis='y', colors='tab:blue')
+    ax2.spines['left'].set_color('tab:blue')
+    ax2.spines['right'].set_color('tab:gray')
+    ax2.spines['top'].set_color('tab:gray')
+    ax2.spines['bottom'].set_color('tab:gray')
+
+    if show_density:
+      amp = self.iClamper.Amplitude
+      ylabel = r'pA/$\mu$$m^2$'
+    else:
+      amp = self.iClamper.Amplitude*self.Surface/1000.
+      ylabel = "nA"
+
+    if amp < 0:
+      y0, ym = 1.1*amp, -0.1*amp
+    elif amp < 0.6:
+      y0 = -0.05*amp
+      ym = 1.1*amp
+    else:
+      y0 = -0.1*amp
+      ym = 1.1*math.ceil(amp)
+    ax2.set_ylim([y0, ym])
+    ax2.set_ylabel(ylabel)
+    plt.subplots_adjust(hspace=0.5)
+    plt.show()
+
+  def plot_Vm(self, ylim=None, show_density=True):
+    dt = self.T[-1]/(len(self.T)-1)
+    xlim = (self.T[0],self.T[-1]+dt)
+    M, m = max(self.Vm), min(self.Vm)
+    if ylim == None and M > m:
+      R = (M - m)/10
+      ylim = [m-R,M+R]
+
+    if self.iClamper:
+      self._plot_Vm_clamper(xlim, ylim, show_density)
+    else:
+      self._plot_Vm(xlim, ylim, show_density)
