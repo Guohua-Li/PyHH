3b6e2e9a657cc7f212633a3b5e0133dc8f2122b0
Guohua-Li <licharles35@gmail.com>
2023-06-21 10:37:07 -0400

    Update voltage-clamp-3.py

--- voltage-clamp-3.py
+++ voltage-clamp-3.py
@@ -1,27 +1,20 @@
 from compartment import Compartment
 from gating import KDR
-from clampers import VClamper, Rect
+from clampers import VClamper
 from experiment import Experiment
 
-channels = {
-    KDR: [0.18,-90],
-}
+channels = {  KDR: [0.18,-90],  }
 
 cell = Compartment(50, None, channels)
 
-clamper = VClamper(-60)
-clamper.Waveform = Rect(delay=5, width=20, amplitude=0)
-clamper.clamp(cell)
+clamper = VClamper(cell, -60)
 
 xp = Experiment(cell)
 xp.run(t=30)
 
-clamper.plot()
+clamper.plot_current()
 
-clamper.Waveform = Rect(delay=2, width=20, amplitude=40)
+clamper.set_waveform('rect', delay = 2, width=20, amplitude=40)
 xp.Clock = 0
 xp.run(t=30)
-clamper.plot()
-#clamper.plot(show_Jm=1,show_Jn=0) # show Jm, not Jn
-#clamper.plot(show_Jm=1,show_Jn=0,show_Jp=1) # show Jm, Jp, but not Jn
-
+clamper.plot_current()
