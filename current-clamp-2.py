fdd0eb16cba4110ed228e068ca36220db3c7c1be
Guohua-Li <licharles35@gmail.com>
2023-06-21 10:25:27 -0400

    Update current-clamp-2.py

--- current-clamp-2.py
+++ current-clamp-2.py
@@ -1,23 +1,45 @@
+"""
+In this tutorial, we are going to inject a rectangle current pulse 
+to see the changes in membrane potentials. For this purpose, 
+we need to import the IClamper and the Rect classes.
+
+"""
+
 from compartment import Compartment
-from gating import NaT, KDR, gL
-from clampers import IClamper, Rect # IClamoer: Current clamper,  Rect: rectangular waveform for stimulation
 from experiment import Experiment
+from gating import NaT, KDR, gL
+from clampers import IClamper
+from ploting import plot_recording
 
 
-cell = Compartment(diameter=40, length=None)
 
-channels = {
+
+chann_dist = {
     NaT: [0.6,  50],
     KDR: [0.18,-90],
     gL:  [0.03,-60]
 }
 
-cell.add_channels(channels)
+cell = Compartment(diameter=40, length=None, channels = chann_dist)
 
-clamper = IClamper()
-clamper.Waveform = Rect(delay=1, width=1, amplitude=0.55)
-clamper.clamp(cell)
+clamper = IClamper(cell) # first, we define a clamper
+clamper.set_waveform('rect', delay = 2.5, width=1)
 
 xp = Experiment(cell)
-xp.run(t = 10)
-cell.plot_Vm(vlimit=[-80, 20])
+
+
+VmTraces = [] # store potential from each run
+StTraces = [] # store stimulus from each run
+amplitudes = [-0.5, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.52, 0.54, 0.56]
+
+for amp in amplitudes:
+    clamper.set_amplitude(amp)
+    xp.Clock = 0
+    xp.run(t = 20)
+    VmTraces.append(cell.Vm)
+    StTraces.append(clamper.Command)
+
+
+
+
+plot_recording(xp.T, VmTraces, StTraces)
