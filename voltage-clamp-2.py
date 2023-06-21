f4ebce1fbdddf927596519a8c1fa908e5315cfaf
Guohua-Li <licharles35@gmail.com>
2023-06-21 10:36:14 -0400

    Update voltage-clamp-2.py

--- voltage-clamp-2.py
+++ voltage-clamp-2.py
@@ -1,6 +1,6 @@
 from compartment import Compartment
 from gating import NaT
-from clampers import VClamper, Rect
+from clampers import VClamper
 from experiment import Experiment
 
 channels = {
@@ -9,18 +9,14 @@ channels = {
 
 soma = Compartment(50, None, channels)
 
-# another way of adding a clamper:
+
 clamper = soma.add_vclamper(-60)
-clamper.Waveform = Rect(delay=5, width=20, amplitude=0)
+
 
 xp = Experiment(soma)
 xp.run(30)
-clamper.plot()
+clamper.plot_current()
 
-clamper.Waveform = Rect(delay=2, width=20, amplitude=40) # amplitude relative to the baseline
-xp.Clock = 0
+clamper.set_waveform('rect', delay = 2, width=20, amplitude=40)
 xp.run(t=30, dt=0.002) # we set the step to be 0.002
-clamper.plot()
-
-#clamper.plot(show_Jm=1,show_Jn=0) # show Jm, not Jn, need more explaination
-#clamper.plot(show_Jm=1,show_Jn=0,show_Jp=1) # show Jm, Jp, but not Jn
+clamper.plot_current()
